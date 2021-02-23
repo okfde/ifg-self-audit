77571b67ae41e1025bdbde9801480de4a0111dda
krmax44 <hi@krmax44.de>
2021-02-23 10:45:28 +0100

    fix: drop resize observer

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,12 @@
 </template>
 
 <script>
-import { ResizeObserver } from '@juggle/resize-observer';
-
 import SiteHeader from './components/SiteHeader';
 import QuestionnaireView from './components/QuestionnaireView';
 
 export default {
   name: 'App',
-  components: { SiteHeader, QuestionnaireView },
-  mounted() {
-    this.resize();
-
-    const ro = new ResizeObserver(this.resize);
-    ro.observe(document.body);
-    document.addEventListener('resize', this.resize);
-  },
-  methods: {
-    resize() {
-      const { offsetHeight } = document.body;
-      window.parent.postMessage(
-        ['setIframeHeight', 'self-audit-iframe', offsetHeight],
-        '*'
-      );
-    }
-  }
+  components: { SiteHeader, QuestionnaireView }
 };
 </script>
 
